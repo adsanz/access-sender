8b34b1cc1984012cd884fe67fb9aa72211b8458a
Adrian Sanz <asanz@mrmilu.com>
2022-09-23 12:43:49 +0200

    Init

---
+++ README.md
@@ -0,0 +1,69 @@
+# Access Sender
+
+This script allows a user to:
+
+1. Get a secret from secret-manager, or a command line arg, or a file
+2. Create a secret on onetimesecret with that content
+3. Send it on slack if user is provided 
+
+## Requirements
+
+On the script, you shall fill this variables:
+- TOKEN_OTS: holds the token for your OTS account
+- EMAIL_OTS: holds the email of your OTS account
+- TOKEN_SLACK: holds the token of the "Bot user token" from an slack app
+- AWS_PROFILE: AWS profile used to gather secret info from secret manager
+- AWS_REGION: AWS region where we are going to search those secrets
+
+## Usage
+
+I'll show usage via examples:
+
+1. Gathering a secret from AWS and sending it via slack
+
+```bash
+❯ python3 ez.py -n "dev-db-auth" -u "example@example.com" -m "DB details:  "
+23-Sep-22 11:56:27 - INFO - Found credentials in shared credentials file: ~/.aws/credentials
+23-Sep-22 11:56:29 - INFO - Onetime secret: https://onetimesecret.com/secret/cdsaf3arfesf3wre2a3e24rat
+23-Sep-22 11:56:29 - INFO - User id -> UY652SRT6 from user -> example@example.com
+23-Sep-22 11:56:29 - INFO - Slack message sent to user -> example@example.com
+```
+
+2. Secret from command line
+
+```bash
+❯ python3 ez.py -s "General Kenobi" -u "example@example.com" -m "Hello there! "
+23-Sep-22 11:59:58 - INFO - Found credentials in shared credentials file: ~/.aws/credentials
+23-Sep-22 11:59:58 - INFO - Onetime secret: https://onetimesecret.com/secret/cdsaf3arfesf3wre2a3e24rat
+23-Sep-22 11:59:59 - INFO - User id -> UY652SRT6 from user -> example@example.com
+23-Sep-22 11:59:59 - INFO - Slack message sent to user -> example@example.com
+```
+
+3. Secret from a file
+
+```bash
+❯ cat test
+Hello
+❯ python3 ez.py -s $(pwd)/test -u "example@example.com" -m "Hello there! "
+23-Sep-22 12:01:40 - INFO - Found credentials in shared credentials file: ~/.aws/credentials
+23-Sep-22 12:01:40 - INFO - Reading secret from file: /home/adsanz/projects/disco_sec/development/access-sender/test
+23-Sep-22 12:01:41 - INFO - Onetime secret: https://onetimesecret.com/secret/cdsaf3arfesf3wre2a3e24rat
+23-Sep-22 12:01:41 - INFO - User id -> UY652SRT6 from user -> example@example.com
+23-Sep-22 12:01:41 - INFO - Slack message sent to user -> example@example.com
+```
+
+4. Not sending the message via slack, just generate the secret
+
+```bash
+❯ python3 ez.py -s "Hello there" -m "Bye"
+23-Sep-22 12:03:26 - INFO - Found credentials in shared credentials file: ~/.aws/credentials
+23-Sep-22 12:03:26 - INFO - Onetime secret: https://onetimesecret.com/secret/cdsaf3arfesf3wre2a3e24rat
+```
+
+## Features
+
+PR's are welcome!
+
+## Notes
+
+Tested with Python 3.8
---
+++ access-sender.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+
+
+import boto3, yaml, json, requests, argparse, logging
+from requests.auth import HTTPBasicAuth
+
+TOKEN_OTS='PLACEHOLDER'
+EMAIL_OTS='PLACEHOLDER'
+TOKEN_SLACK='PLACEHOLDER'
+AWS_PROFILE='PLACEHOLDER'
+AWS_REGION='PLACEHOLDER'
+
+# Logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+# boto secret manager client with profile
+session = boto3.Session(profile_name=AWS_PROFILE, region_name=AWS_REGION)
+sm = session.client('secretsmanager')
+
+def SecretReader(sm,secret_name):
+    """
+    Return in a nicely yaml format the secret
+    """
+
+    secret = sm.get_secret_value(SecretId=secret_name)
+    return yaml.dump(json.loads(secret['SecretString']), default_flow_style=False)
+
+def OneTimeSecretCreate(secret,api_token, user):
+    """
+    Create and print secret creation, return secret key
+    """
+    req = requests.post('https://onetimesecret.com/api/v1/share', 
+    auth=HTTPBasicAuth(user, api_token),
+    data={
+        'secret': secret,
+        'ttl': 604800,})
+    logging.debug(yaml.dump(req.json(), default_flow_style=False))
+    return req.json()['secret_key']
+
+def SlackUserLookup(token,user):
+    """
+    Lookup user id
+    """
+    headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'Authorization': 'Bearer ' + token}
+    payload = {
+        "email": user
+    }
+    response = requests.post('https://slack.com/api/users.lookupByEmail', data=payload, headers=headers)
+    return response.json()['user']['id']
+
+def SlackMessage(token,user_id,message,secret_key):
+    """
+    Send the access via slack
+    """
+    headers = {'Content-Type': 'application/json; charset=UTF-8', 'Authorization': 'Bearer ' + token}
+    payload = {
+        "channel": user_id,
+        "text": message+'https://onetimesecret.com/secret/'+secret_key,
+        "as_user": True
+    }
+    response = requests.post('https://slack.com/api/chat.postMessage', json=payload, headers=headers)
+    return response.json()
+
+
+parser = argparse.ArgumentParser(description='This script will create a secret in OneTimeSecret and send it to a user via Slack if email is provided')
+parser.add_argument('-s','--secret', help='Secret to be send (do not use -n and -s at the same time)', required=False)
+parser.add_argument('-n','--name', help='Secret name from AWS (name of the to be retrieved)', required=False)
+parser.add_argument('-u','--user', help='Email for the user where we are going to send the secret', required=False)
+parser.add_argument('-m','--message', help='Message to be sent', required=True)
+args = vars(parser.parse_args())
+
+logging.debug(args)
+
+if args['name']:
+    secret_name = args['name']
+    logging.info('Reading secret: '+secret_name)
+    secret = SecretReader(sm,secret_name)
+elif args['secret']:
+    secret = args['secret']
+    #check if secret is a file
+    try:
+        with open(secret, 'r') as f:
+            logging.info('Reading secret from file: '+secret)
+            secret = f.read()
+    except FileNotFoundError:
+        pass
+else:
+    raise Exception('You need to provide a secret or a secret name')
+
+
+secret_key = OneTimeSecretCreate(secret,TOKEN_OTS, EMAIL_OTS)
+logging.info('Onetime secret: https://onetimesecret.com/secret/'+secret_key)
+if args['user']:
+    user_id = SlackUserLookup(TOKEN_SLACK,args['user'])
+    logging.info('User id -> {} from user -> {}'.format(user_id,args['user']))
+    slack_message = SlackMessage(TOKEN_SLACK,user_id,args['message'],secret_key)
+    logging.debug(yaml.dump(slack_message, default_flow_style=False))
+    logging.info('Slack message sent to user -> {}'.format(args['user']))
